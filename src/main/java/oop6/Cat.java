69f706baa03a43215941a5bf94548ef3291b1588
AABakotin <megabart57@gmail.com>
2021-06-29 14:22:46 +0300

    oop6

---
+++ src/main/java/oop6/Cat.java
@@ -0,0 +1,24 @@
+package oop6;
+
+public class Cat extends Animal{
+
+    public Cat(String name, String color){
+        super (name, color);
+   }
+    public void animalRun(int disOnRoad, int disOnWater){
+       if (disOnRoad > 200)
+            System.out.printf("Котик %s не может пробежать большое расстояние\n" , this.name);
+       if (disOnRoad <= 200)
+            System.out.printf("Котик %s лекго пробежал дистанцию в %d метров\n" , this.name, disOnRoad);
+        if (disOnWater>=0)
+            System.out.printf("Котик %s не плавает!!!!\n" , this.name);
+
+    }
+
+
+    @Override
+    public void voice(){
+        System.out.printf("Кот %s подает голос\n", this.name);
+    }
+
+}
---
+++ src/main/java/oop6/Dog.java
@@ -0,0 +1,32 @@
+package oop6;
+
+public class Dog extends Animal {
+
+    public Dog(String name, String color) {
+        super(name, color);
+    }
+
+    public void animalRun(int disOnRoad, int disOnWater) {
+        if (disOnRoad > 500)
+            System.out.printf("Собачка %s не может пробежать такое расстояние\n" , this.name);
+        if (disOnRoad < 500)
+            System.out.printf("Собачка %s пробежали %d метров\n" , this.name, disOnRoad);
+        if (disOnWater < 10)
+            System.out.printf("Собака %s проплыла %d метров\n" , this.name, disOnWater);
+        if (disOnWater > 10)
+            System.out.printf("Собаки %s не могут плыть более 10 метров!!!!\n", this.name);
+
+    }
+
+
+
+
+
+
+
+        @Override
+        public void voice(){
+            System.out.printf("Собака %s подает голос\n", this.name);
+        }
+
+}
